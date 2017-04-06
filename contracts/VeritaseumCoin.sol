8e1595aa6be6bce0f47a89bd09eb689f0e51f77c
Ryan F Venter <msg@rfv.io>
2017-04-06 17:16:46 +0800

    Moved setPrice into the crowdsale contract. Changed PRICE to price and set to public. Removed setPrice as solidity will automatically create setter functions for public globals. Fixed the tabs to 4 spaces (from 2).

--- contracts/VeritaseumCoin.sol
+++ contracts/VeritaseumCoin.sol
@@ -9,11 +9,6 @@ contract VeritaseumCoin is CrowdsaleToken, Ownable {
     string public symbol = "VERI";
     uint public decimals = 18;
 
-    // replace this with any other price function
-    function setPrice(uint _price) onlyOwner {
-      	PRICE = _price;
-    }
-
     // withdraw Ether
     function withdrawEther() payable onlyOwner returns (bool) {
         return owner.send(this.balance);
@@ -23,5 +18,10 @@ contract VeritaseumCoin is CrowdsaleToken, Ownable {
         if(_value <= 0) throw;                                      // Check send token value > 0;
     	balances[_to] += _value;
     	totalSupply += _value;
+    }
+
+    // replace this with any other price function
+    function setPrice(uint _price) onlyOwner {
+        price = _price;
     }
 }
--- installed_contracts/zeppelin/contracts/token/CrowdsaleToken.sol
+++ installed_contracts/zeppelin/contracts/token/CrowdsaleToken.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.4.8;
 
 import "./StandardToken.sol";
 
-
 /*
  * CrowdsaleToken
  *
@@ -11,30 +10,40 @@ import "./StandardToken.sol";
  */
 contract CrowdsaleToken is StandardToken {
 
-  string public name = "CrowdsaleToken";
-  string public symbol = "CRW";
-  uint public decimals = 18;
+    uint public startTime = 1492435800; // 2017/04/17 09:30 UTC-5
+    uint public closeTime = startTime + 31 days;
 
-  // 1 ether = 500 example tokens 
-  uint internal PRICE = 500;
+    // 1 ETH = 50 tokens (^E18). 
+    uint public price = 50 ether;       // Each token has 18 decimal places, just like ether. 
 
-  function () payable {
-    createTokens(msg.sender);
-  }
-  
-  function createTokens(address recipient) payable {
-    if (msg.value == 0) {
-      throw;
+    function () payable {
+        createTokens(msg.sender);
     }
-
-    uint tokens = safeMul(msg.value, getPrice());
-
-    totalSupply = safeAdd(totalSupply, tokens);
-    balances[recipient] = safeAdd(balances[recipient], tokens);
-  }
   
-  // replace this with any other price function
-  function getPrice() constant returns (uint result) {
-    return PRICE;
-  }
+    function createTokens(address recipient) payable {
+        uint currentPrice;
+
+        if ((now < startTime) || (now > closeTime) || (msg.value == 0)) throw;
+
+        if (now < (startTime + 1 days)) {
+            currentPrice = safeDiv(safeMul(price, 8), 10);  // 20 % discount (x * 8 / 10)
+        } 
+        else if (now < (startTime + 2 days)) {
+            currentPrice = safeDiv(safeMul(price, 9), 10);  // 10 % discount (x * 9 / 10)
+        }
+        else if (now < (startTime + 12 days)) {
+            // 1 % reduction in the discounted rate from day 2 until day 12 (sliding scale per second)
+            // 8640000 is 60 x 60 x 24 x 100 (100 for 1%) (60 x 60 x 24 for seconds per day)
+            currentPrice = price - safeDiv(safeMul(startTime + 12 days - now, price), 8640000);
+        }
+        else {
+            currentPrice = price;
+        }
+
+        uint tokens = safeMul(msg.value, currentPrice);
+
+        totalSupply = safeAdd(totalSupply, tokens);
+        balances[recipient] = safeAdd(balances[recipient], tokens);
+    }
+  
 }
