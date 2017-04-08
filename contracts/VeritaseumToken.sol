3e3a3f567d7270cf49a3037fe948f50ce5030e79
Ryan F Venter <msg@rfv.io>
2017-04-08 08:08:36 +0800

    Fixed totalSupply from 100 to 100 Million. changed createTokens to purchaseTokens. Killable inheritance. added allocationRatio check to ensure that ICO purchasers may only purchase up to 51% of the totalSupply of tokens.

--- contracts/VeritaseumToken.sol
+++ contracts/VeritaseumToken.sol
@@ -4,7 +4,7 @@ import 'zeppelin/token/StandardToken.sol';      // ERC20 Standard Token interfac
 import 'zeppelin/ownership/Ownable.sol';        // set specific function for owner only
 import 'zeppelin/lifecycle/Killable.sol';       // kill feature for contract
 
-contract VeritaseumToken is Ownable, StandardToken {
+contract VeritaseumToken is Ownable, StandardToken, Killable {
 
     string public name = "Veritaseum";
     string public symbol = "VERI";
@@ -13,21 +13,24 @@ contract VeritaseumToken is Ownable, StandardToken {
     uint public startTime = 1493130600;         // 2017 April 25th 9:30 EST (14:30 UTC)
     uint public closeTime = startTime + 31 days;
     uint public price = 30 ether;               // Each token has 18 decimal places, just like ether. 1 ETH = Ve 30 tokens (^E18). 
-    uint public totalSupply = 100 ether;        // total supply of 100 Million Tokens
+    uint public totalSupply = 100000000 ether;  // total supply of 100 Million Tokens
+    uint public allocationRatio = 51;           // the totalSupply ratio of tokens allocated towards the ICO
 
     function VeritaseumToken() {
         balances[msg.sender] = totalSupply;     // allocate all initial tokens to the owner
     }
 
     function () payable {
-        createTokens(msg.sender);
+        purchaseTokens(msg.sender);
     }
     
-    function createTokens(address recipient) payable {
-        uint currentPrice;
-
+    function purchaseTokens(address recipient) payable {
+        // check if now is within ICO period, or if the amount sent is nothing
         if ((now < startTime) || (now > closeTime) || (msg.value == 0)) throw;
-
+        
+        // the check for the allocation ratio can only be done after the currentPrice has been determined below
+        
+        uint currentPrice;
         if (now < (startTime + 1 days)) {
             currentPrice = safeDiv(safeMul(price, 8), 10);  // 20 % discount (x * 8 / 10)
         } 
@@ -37,17 +40,25 @@ contract VeritaseumToken is Ownable, StandardToken {
         else if (now < (startTime + 12 days)) {
             // 1 % reduction in the discounted rate from day 2 until day 12 (sliding scale per second)
             // 8640000 is 60 x 60 x 24 x 100 (100 for 1%) (60 x 60 x 24 for seconds per day)
-            currentPrice = price - safeDiv(safeMul(startTime + 12 days - now, price), 8640000);
+            currentPrice = safeSub(price, safeDiv(safeMul(startTime + 12 days - now, price), 8640000));
         }
         else {
             currentPrice = price;
         }
-
         uint tokens = safeMul(msg.value, currentPrice);
 
-        // transfer tokens from owner account to purchasers account
-        balances[recipient] = safeAdd(balances[recipient], tokens);
-        balances[owner] = safeSub(balances[owner], tokens);
+        // check if this purchase will go over the allowed ICO allocation ratio
+        // current ICO ownership: totalSupply - balances[owner]
+        // ICO may have up to allocationRatio of totalSupply: totalSupply / allocationRatio
+        if ((totalSupply - balances[owner] + tokens) <= safeDiv(totalSupply, allocationRatio)) {
+            // transfer tokens from owner account to purchasers account
+            balances[recipient] = safeAdd(balances[recipient], tokens);
+            balances[owner] = safeSub(balances[owner], tokens);
+        }
+        else {
+            // return the Ether
+            throw;
+        }
     }
 
     // withdraw Ether
